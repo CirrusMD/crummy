e47378543fc323647ead3a99dedc00f202609d11
Cory Boyd <cory-boyd@cirrusmd.com>
2021-06-17 15:53:40 -0600

    Fix autoload issues with Rails 6

--- lib/crummy/railtie.rb
+++ lib/crummy/railtie.rb
@@ -5,14 +5,18 @@ module Crummy
   class Railtie < Rails::Railtie
     initializer "crummy.action_controller" do |app|
       if defined?(ActionController)
-        require 'crummy/action_controller'
-	ActionController::Base.send :include,  Crummy::ControllerMethods 
+        ActiveSupport.on_load(:action_controller) do
+          require 'crummy/action_controller'
+          ActionController::Base.send :include,  Crummy::ControllerMethods
+        end
       end
     end
     
     initializer "crummy.action_view" do |app|
-      require 'crummy/action_view'
-      ActionView::Base.send :include, Crummy::ViewMethods
+      ActiveSupport.on_load(:action_view) do
+        require 'crummy/action_view'
+        ActionView::Base.send :include, Crummy::ViewMethods
+      end
     end
   end
 end
